6ef2a10d750cbb368088bc85e66a9e8087c678d6
Shulhi Sapli <shulhi@gmail.com>
2013-07-16 21:27:57 +0800

    get available networks

--- BetterNetwork/ViewModels/AppViewModel.cs
+++ BetterNetwork/ViewModels/AppViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,32 @@ namespace BetterNetwork.ViewModels
 {
     public class AppViewModel : PropertyChangedBase
     {
+        public void GetAvailableNetworks()
+        {
+            var registry = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            var interfaces = registry.OpenSubKey(@"SOFTWARE\Microsoft\WlanSvc\Interfaces\");
+            var profiles = interfaces.OpenSubKey(interfaces.GetSubKeyNames().First() + @"\Profiles");
+
+            foreach (var profile in profiles.GetSubKeyNames())
+            {
+                var metadata = profiles.OpenSubKey(profile + @"\Metadata").GetValue("Channel Hints");
+                var network = ExtractNetworkNames((byte[]) metadata);
+            }
+        }
+        
+        private static string ExtractNetworkNames(byte[] metadata)
+        {
+            var position = 0;
+            for (var i = 4; i < metadata.Length; i++)
+            {
+                if (metadata[i] == 0)
+                {
+                    position = i;
+                    break;
+                }
+            }
+
+            return Encoding.ASCII.GetString((byte[])metadata, 4, position - 4);
+        }
     }
 }
