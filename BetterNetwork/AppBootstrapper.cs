2b4bc486b8e2b626d0b602ad1d2ea824226fe42b
Shulhi Sapli <shulhi@gmail.com>
2013-07-16 18:33:22 +0800

    added CM

---
+++ BetterNetwork/AppBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BetterNetwork.ViewModels;
+using Caliburn.Micro;
+
+namespace BetterNetwork
+{
+    public class AppBootstrapper : Bootstrapper<AppViewModel>
+    {
+    }
+}
