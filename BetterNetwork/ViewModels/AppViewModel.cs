dec91d8b39b510f81b346e24eb59cd78d7b90625
Shulhi Sapli <shulhi@gmail.com>
2013-07-17 14:29:17 +0800

    Added events to handle checkboxes

--- BetterNetwork/ViewModels/AppViewModel.cs
+++ BetterNetwork/ViewModels/AppViewModel.cs
@@ -1,4 +1,8 @@
-﻿using Caliburn.Micro;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using BetterNetwork.Models;
+using Caliburn.Micro;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -10,20 +14,51 @@ namespace BetterNetwork.ViewModels
 {
     public class AppViewModel : PropertyChangedBase
     {
-        public void GetAvailableNetworks()
+        #region Properties
+        private ObservableCollection<InterfaceProfile> _interfaceProfiles; 
+        public ObservableCollection<InterfaceProfile> InterfaceProfiles
         {
-            var profileReg = new Dictionary<string, string>();
-
-            var registry = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
-            var interfaces = registry.OpenSubKey(@"SOFTWARE\Microsoft\WlanSvc\Interfaces\");
-            var profiles = interfaces.OpenSubKey(interfaces.GetSubKeyNames().First() + @"\Profiles");
-
-            foreach (var profile in profiles.GetSubKeyNames())
+            get { return _interfaceProfiles; } 
+            set
             {
-                var metadata = profiles.OpenSubKey(profile + @"\Metadata").GetValue("Channel Hints");
-                var network = ExtractNetworkNames((byte[]) metadata);
-                var path = profiles.Name + "\\" + profile;
-                profileReg.Add(network, path);
+                _interfaceProfiles = value;
+                NotifyOfPropertyChange(() => InterfaceProfiles);
+            }
+        }
+
+        private ObservableCollection<InterfaceProfile> ToBeDeletedInterfaces { get; set; }
+        #endregion
+
+        public AppViewModel()
+        {
+            InterfaceProfiles = new ObservableCollection<InterfaceProfile>();
+            ToBeDeletedInterfaces = new ObservableCollection<InterfaceProfile>();
+        }
+
+        public void GetInterfaceProfiles()
+        {
+            try
+            {
+                var registry = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                var interfaces = registry.OpenSubKey(@"SOFTWARE\Microsoft\WlanSvc\Interfaces\");
+                var profiles = interfaces.OpenSubKey(interfaces.GetSubKeyNames().First() + @"\Profiles");
+
+                foreach (var profile in profiles.GetSubKeyNames())
+                {
+                    var metadata = profiles.OpenSubKey(profile + @"\Metadata").GetValue("Channel Hints");
+                    var network = ExtractNetworkNames((byte[]) metadata);
+                    var path = profiles.Name + "\\" + profile;
+
+                    InterfaceProfiles.Add(new InterfaceProfile {Name = network, RegistryPath = path});
+                }
+
+                registry.Close();
+                interfaces.Close();
+                profiles.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
         
@@ -41,5 +76,25 @@ namespace BetterNetwork.ViewModels
 
             return Encoding.ASCII.GetString((byte[])metadata, 4, position);
         }
+
+        #region Events Handler
+        public void NetworkChecked(RoutedEventArgs e)
+        {
+            var item = e.Source as CheckBox;
+
+            var profile = InterfaceProfiles.First(x => x.Name == (string)item.Content);
+            if (!ToBeDeletedInterfaces.Contains(profile))
+                ToBeDeletedInterfaces.Add(profile);
+        }
+
+        public void NetworkUnchecked(RoutedEventArgs e)
+        {
+            var item = e.Source as CheckBox;
+
+            var profile = InterfaceProfiles.First(x => x.Name == (string)item.Content);
+            if (ToBeDeletedInterfaces.Contains(profile))
+                ToBeDeletedInterfaces.Remove(profile);
+        }
+        #endregion
     }
 }
